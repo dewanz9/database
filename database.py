4707fff9768610cff34d09ebe2fa111ad9008e9b
dewanz9 <connor.dewar@gmail.com>
2016-07-19 15:30:22 +1200

    added more files. mainly a backup

--- database.py
+++ database.py
@@ -4,6 +4,11 @@ The database class file
 
 class DataBase(object):
 	"""The database class for creating an empty database"""
-	def __init__(self, *columns):
-		print(columns)
+	def __init__(self, name, *columns):
+		self.columns = list(columns)
+		self.rows = []
+		self.name = name
+	def save(self):
+		"""saves the database to name.txt under standard format"""
+		pass
 
